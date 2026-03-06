f75b2e108efe8797bf2a0a7f62083a3490aefaf8
M-Talha183 <shahzainali5002@gmail.com>
2026-03-06 06:48:09 +0500

    Add some practice question in javascript folder related to if else

---
+++ javascript/assignment/app.js
@@ -0,0 +1,59 @@
+// Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights
+
+// ************************************ Code ***************************
+//var city= prompt("Enter your city name ")
+
+// if (city == "karachi"){
+//     console.log("Welcome TO  City OF Lights")
+// }
+
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+
+// ************************************ Code ***************************
+
+// var gender = prompt("Enter you Gender")
+
+// if (gender == "male"){
+//     console.log("Good Morinig sir ")
+// }else{
+//     console.log("Good morning Madam ")
+// }
+
+// ************************************ Code ***************************
+// Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+// Signal color Message
+// Red Must Stop
+// Yellow Ready to move
+// Green Move now
+// 4. Write 
+
+// var traficsignal = prompt("Enter a trafic signal color ")
+
+// if (traficsignal == "red"){
+//     console.log("Must Stop")
+// }
+// else if (traficsignal == "Yellow "){
+// console.log("Ready to go")
+// }
+// else if(traficsignal == "green"){
+//     console.log("Moved NOw ")
+// }
+// else{
+//     console.log("Enter the valid color ")
+// }
+
+// 4. Write a program to take input remaining fuel in car (in
+// litres) from user. If the current fuel is less than 0.25litres,
+// show the message “Please refill the fuel in your car”
+// ************************************ Code **************************
+// let fuel = prompt("Enter your fuel in remaing (In leters ) ")
+// if (fuel <= 0.25){
+//     console.log("Please Refuel your Car ")
+// }
+
+// ************************************ Code **************************
