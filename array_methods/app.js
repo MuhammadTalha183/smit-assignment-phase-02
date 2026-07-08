330db1b1d18d5f7df89269c25fc47f90f83bdd49
M-Talha183 <ullahehsan016@gmail.com>
2026-07-08 22:22:31 +0500

    solve some basic question of filter function

--- array_methods/app.js
+++ array_methods/app.js
@@ -49,3 +49,71 @@ const prices = [100,200,300];
 const dollers = prices.map(price => (price / 280).toFixed(2));
 
 console.log(dollers)
+
+
+// ************************* filter() ********************************
+// Q6
+
+// Keep only even numbers.
+
+const numberss = [2,5,8,9,12,15];
+
+const evenNumbers = numberss.filter(even => even % 2 == 0);
+
+console.log(evenNumbers)
+
+
+// Q7
+
+// Keep only students older than 20.
+
+const studentss = [
+{name:"Ali",age:20},
+{name:"Ahmed",age:23},
+{name:"Sara",age:18}
+];
+
+const maxAge = studentss.filter(age => age.age > 20);
+
+console.log(maxAge);
+
+
+// Q8
+
+// Filter products that cost more than 1000.
+
+const products=[
+{name:"Laptop",price:90000},
+{name:"Mouse",price:800},
+{name:"Keyboard",price:1500}
+];
+
+
+const expensiveProducts = products.filter(product => product.price > 1000);
+console.log(expensiveProducts);
+
+
+// Q9
+
+// Filter only active users.
+
+const users=[
+{name:"Ali",active:true},
+{name:"Ahmed",active:false},
+{name:"Sara",active:true}
+];
+
+const activeuser = users.map(active => active.active == true);
+
+console.log(activeuser)
+
+
+// Q10
+
+// Keep only words longer than 5 characters.
+
+const words=["cat","elephant","car","computer","pen"];
+
+const char = words.filter(char => char.length > 5);
+
+console.log(char);
